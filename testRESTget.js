4724efdbcdf3a358c6f244bb358d97802f5784bc
MatthewHoque <hoque.matt@gmail.com>
2022-03-03 19:09:45 -0800

    testRestget.js refined

--- testRESTget.js
+++ testRESTget.js
@@ -6,16 +6,32 @@ async function main() {
     let tkd = await credManager.supplyTokenData()
     console.log(tkd.access_token + "| async")
 
+
+
+    var body = {
+        q: "valentino khan",
+        type: ["artist"],
+        market: 'US',
+        limit: 5,
+        offset: 0,
+        // include_external: 'true',
+    }
+
+    var qString = "?" + Object.keys(body).map(key => key + '=' + body[key]).join('&');
+    qString = qString.replace(" ", "%20")
+    console.log(qString)
+
+
     const options = {
         hostname: 'api.spotify.com',
         port: 443,
-        path: '/v1/users/jmperezperez',
+        path: '/v1/search' + qString,
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer ' + tkd.access_token
+            'Authorization': 'Bearer ' + tkd.access_token,
+            'Content-Type': 'application/json',
         }
     }
-
     // request object
     var req = https.request(options, function (res) {
         var result = '';
@@ -23,7 +39,7 @@ async function main() {
             result += chunk;
         });
         res.on('end', function () {
-            console.log(result+"RESULT");
+            console.log(result);
         });
         res.on('error', function (err) {
             console.log(err);
@@ -34,9 +50,8 @@ async function main() {
     req.on('error', function (err) {
         console.log(err);
     });
+    // req.write(qString)
     req.end();
 
 }
-
-
 main()
